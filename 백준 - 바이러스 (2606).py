c9498abf8d3e1e95546f487f3f1b43fc2ab707ad
besthong <rjsekaskfro@naver.com>
2022-02-24 20:28:48 +0900

    .

---
+++ 백준 - 바이러스 (2606).py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+
+graph=[[] for _ in range(n+1)]
+
+for _ in range(m):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(graph, v, visited):
+    visited[v] = True
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph,i,visited)
+
+    return visited
+
+visited=[False]*(n+1)
+dfs(graph,1,visited)
+print(visited.count(True)-1)
---
+++ 파이썬 - DFS와 BFS (1260).py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n,m,v = map(int,input().split())
+graph = [[] for _ in range(n+1)]
+for i in range(m):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1,n+1):
+    graph[i].sort()
+
+def dfs(graph,v,visited):
+    visited[v] = True
+    print(v,end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph,i,visited)
+
+def bfs(graph,v,visited):
+    q = deque([v])
+    visited[v] = True
+
+    while q:
+        cur = q.popleft()
+        print(cur, end=' ')
+
+        for i in graph[cur]:
+            if not visited[i]:
+                q.append(i)
+                visited[i]=True
+
+
+visited=[False]*(n+1)
+dfs(graph,v,visited)
+print()
+visited=[False]*(n+1)
+bfs(graph,v,visited)
